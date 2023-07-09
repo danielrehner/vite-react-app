e5de847c8d62003c103563370c0f260b4b2e4ec7
Daniel Rehner <danielrehner@gmail.com>
2023-07-09 11:41:01 -0500

    Add list group component with destructured props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import Message from './Message';
+import ListGroup from "./components/ListGroup";
+
+let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
 function App() {
-  return <div><Message /></div>;
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" />
+    </div>
+  );
 }
 
 export default App;
